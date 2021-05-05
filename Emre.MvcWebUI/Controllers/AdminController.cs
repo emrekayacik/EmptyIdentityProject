c1133a24daac0031c01ade177b257e2d412d4588
Emre KAYACIK <emrekayacik@eskisehir.edu.tr>
2021-05-05 18:50:33 +0300

    admin and role screens added

---
+++ Emre.MvcWebUI/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using Emre.MvcWebUI.Identity;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Web.Mvc;
+
+namespace Emre.MvcWebUI.Controllers
+{
+    [Authorize(Roles = "admin")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+
+        private UserManager<ApplicationUser> userManager;
+
+        public AdminController()
+        {
+            var userStore = new UserStore<ApplicationUser>(new IdentityDataContext());
+            userManager = new UserManager<ApplicationUser>(userStore);
+
+        }
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(userManager.Users);
+        }
+    }
+}
---
+++ Emre.MvcWebUI/Models/RoleEditModel.cs
@@ -0,0 +1,24 @@
+﻿using Emre.MvcWebUI.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Emre.MvcWebUI.Models
+{
+    public class RoleEditModel
+    {
+        public IdentityRole Role { get; set; }
+        public IEnumerable<ApplicationUser> Members { get; set; }
+        public IEnumerable<ApplicationUser> NonMembers { get; set; }
+    }
+
+
+    public class RoleUpdateModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string[] RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
+}
